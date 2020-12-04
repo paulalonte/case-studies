b40644a7b9bdcef9104f1a49cd8997298f31883e
Paul Alonte <paulalonte@Pauls-MacBook-Pro.local>
2020-12-05 01:46:31 +0800

    updated navigation

--- src/app/navigation/navigation.ts
+++ src/app/navigation/navigation.ts
@@ -1,24 +1,24 @@
 export const ROUTECONFIG: IRouteConfig[] = [
     {
-        path: '',
-        display: 'Home'
-      },
-      {
-        path: '/tasks',
-        display: 'Tasks'
-      },
-      {
-        path: '/users',
-        display: 'Users'
-      },
-      {
-        path: '/assignments',
-        display: 'Assignments'
-      },
-      {
-        path: '/inventory',
-        display: 'Inventory'
-      }
+      path: '',
+      display: 'Home'
+    },
+    {
+      path: '/tasks',
+      display: 'Tasks'
+    },
+    {
+      path: '/users',
+      display: 'Users'
+    },
+    {
+      path: '/assignments',
+      display: 'Assignments'
+    },
+    {
+      path: '/inventory',
+      display: 'Inventory'
+    }
 ];
 
 export interface IRouteConfig {
